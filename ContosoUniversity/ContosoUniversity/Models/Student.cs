44659f9dc66398744a1b01c86ed07dbfb5b62998
lorettahammond <lhammo09@baker.edu>
2014-07-08 15:21:39 -0400

    Contoso University
    completed pgs. 126 - 132 (DataType attribute; StringLength Attribute;
    and Column Attribute); verified Student Table

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ContosoUniversity.Models
 {
-public class Student
-{
-public int ID { get; set; }
-public string LastName { get; set; }
-public string FirstMidName { get; set; }
-public DateTime EnrollmentDate { get; set; }
-public virtual ICollection<Enrollment> Enrollments { get; set; }
+    public class Student
+    {
+        public int ID { get; set; }
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
 }
-}
+
+//pg. 127, Contoso University
