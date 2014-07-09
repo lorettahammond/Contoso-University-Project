c820d8212b90755fc65b18f546f999194cf212dd
lorettahammond <lhammo09@baker.edu>
2014-07-09 10:57:35 -0400

    Contoso University Seeding DataBase
    Seeded DataBase with test information, pgs. 148 - 154.

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -1,22 +1,56 @@
-﻿using System;
-using ContosoUniversity.Models;
+﻿using ContosoUniversity.Models;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
+
 namespace ContosoUniversity.DAL
 {
     public class SchoolContext : DbContext
     {
-        public SchoolContext() : base("SchoolContext")
-        {
-        }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)   //For the many-to-many relationship between the Instructor and Course entities, 
+                                                                               //the code specifies the table and column names for the join table. Code First 
+                                                                               //can configure the many-to-many relationship for you without this code, but if 
+                                                                               //you don't call it, you will get default names such as InstructorInstructorID 
+                                                                               //for the InstructorID column.
         {
-    
-    
-     modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Course>()
+            .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+            .Map(t => t.MapLeftKey("CourseID")
+            .MapRightKey("InstructorID")
+            .ToTable("CourseInstructor"));
         }
     }
 }
+
+//pg. 148, Contoso Unversity; replaces code below
+
+
+//using System;
+//using ContosoUniversity.Models;
+//using System.Data.Entity;
+//using System.Data.Entity.ModelConfiguration.Conventions;
+//namespace ContosoUniversity.DAL
+//{
+//    public class SchoolContext : DbContext
+//    {
+//        public SchoolContext() : base("SchoolContext")
+//        {
+//        }
+//        public DbSet<Student> Students { get; set; }
+//        public DbSet<Enrollment> Enrollments { get; set; }
+//        public DbSet<Course> Courses { get; set; }
+//        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+//        {
+    
+    
+//     modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+//        }
+//    }
+//}
+
